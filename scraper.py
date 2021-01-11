ea2dacc14ebc06d734fc79da8588081088ee8370
tertimortymb <kaspar.blumberg@gmail.com>
2021-01-11 17:41:24 +0200

    trying out saving in mysql

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 
-
-#import firebase_admin
+### this is file with mysql database
+### 
+#  #import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import requests
